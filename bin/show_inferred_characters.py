ec762834b0b78eeb6c557d3ff45984cae22188ea
pvanickova <pvanickova@gmail.com>
2019-12-27 15:44:55 +0100

    - updated show inference script to work with 0.6.0 deepspeech model

--- bin/show_inferred_characters.py
+++ bin/show_inferred_characters.py
@@ -5,13 +5,42 @@ import argparse
 import numpy as np
 import os
 import sys
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from tensorflow.contrib.rnn import * # Needed to get support for BlockLSTM
 import tensorflow as tf
+from tensorflow.python.ops import gen_audio_ops as contrib_audio
 
-import util.audio
 from util.text import Alphabet
+
+n_input = 26
+n_context = 9
+n_steps = 16
+n_cell_dim = 2048
+
+
+def samples_to_mfccs(samples, sample_rate):
+    # 16000 = default sample rate
+    # 32 = default feature extraction audio window length in milliseconds
+    audio_window_samples = 16000 * (32 / 1000)
+    # 20 = default feature extraction window step length in milliseconds
+    audio_step_samples = 16000 * (20 / 1000)
+    spectrogram = contrib_audio.audio_spectrogram(samples,
+                                                  window_size=audio_window_samples,
+                                                  stride=audio_step_samples,
+                                                  magnitude_squared=True)
+
+    mfccs = contrib_audio.mfcc(spectrogram, sample_rate, dct_coefficient_count=n_input)
+    mfccs = tf.reshape(mfccs, [-1, n_input])
+
+    return mfccs, tf.shape(input=mfccs)[0]
+
+
+def audiofile_to_features(wav_filename):
+    samples = tf.io.read_file(wav_filename)
+    decoded = contrib_audio.decode_wav(samples, desired_channels=1)
+    features, features_len = samples_to_mfccs(decoded.audio, decoded.sample_rate)
+    return features, features_len
 
 
 def softmax(x):
@@ -19,6 +48,26 @@ def softmax(x):
 
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
+
+
+def create_overlapping_windows(batch_x):
+    batch_size = tf.shape(input=batch_x)[0]
+    window_width = 2 * n_context + 1
+    num_channels = n_input
+
+    # Create a constant convolution filter using an identity matrix, so that the
+    # convolution returns patches of the input tensor as is, and we can create
+    # overlapping windows over the MFCCs.
+    eye_filter = tf.constant(np.eye(window_width * num_channels)
+                               .reshape(window_width, num_channels, window_width * num_channels), tf.float32) # pylint: disable=bad-continuation
+
+    # Create overlapping windows
+    batch_x = tf.nn.conv1d(input=batch_x, filters=eye_filter, stride=1, padding='SAME')
+
+    # Remove dummy depth dimension and reshape into [batch_size, n_windows, window_width, n_input]
+    batch_x = tf.reshape(batch_x, [batch_size, -1, window_width, num_channels])
+
+    return batch_x
 
 
 def run_inference():
@@ -42,77 +91,102 @@ def run_inference():
         args.predicted_character_count = alphabet.size() - 1
 
     # Load frozen graph from file and parse it
-    with tf.gfile.GFile(args.model_file_path, "rb") as f:
-        graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile(args.model_file_path, "rb") as f:
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
+        # print(graph_def.node)
+
 
     with tf.Graph().as_default() as graph:
 
         tf.import_graph_def(graph_def, name="prefix")
 
         # currently hardcoded values used during inference
-        n_input = 26
-        n_context = 9
-        n_steps = 16
 
-        with tf.Session(graph=graph) as session:
-            session.run('prefix/initialize_state')
+        with tf.compat.v1.Session(graph=graph) as session:
 
-            features = util.audio.audiofile_to_input_vector(args.input_file_path, n_input, n_context)
-            num_strides = len(features) - (n_context * 2)
-            window_size = 2 * n_context + 1
+            features, features_len = audiofile_to_features(args.input_file_path)
+            previous_state_c = np.zeros([1, n_cell_dim ])
+            previous_state_h = np.zeros([1, n_cell_dim])
 
-            features = np.lib.stride_tricks.as_strided(
-                features,
-                (num_strides, window_size, n_input),
-                (features.strides[0], features.strides[0], features.strides[1]),
-                writeable=False)
+            # Add batch dimension
+            features = tf.expand_dims(features, 0)
+            features_len = tf.expand_dims(features_len, 0)
 
+            # Evaluate
+            features = create_overlapping_windows(features).eval(session=session)
+            features_len = features_len.eval(session=session)
 
             # we are interested only into logits, not CTC decoding
             inputs = {'input': graph.get_tensor_by_name('prefix/input_node:0'),
+                      'previous_state_c': graph.get_tensor_by_name('prefix/previous_state_c:0'),
+                      'previous_state_h': graph.get_tensor_by_name('prefix/previous_state_h: 0'),
                       'input_lengths': graph.get_tensor_by_name('prefix/input_lengths:0')}
-            outputs = {'outputs': graph.get_tensor_by_name('prefix/logits:0')}
+            outputs = {'outputs': graph.get_tensor_by_name('prefix/logits:0'),
+                       'new_state_c': graph.get_tensor_by_name('prefix/new_state_c:0'),
+                       'new_state_h': graph.get_tensor_by_name('prefix/new_state_h: 0'),
+                       }
 
             logits = np.empty([0, 1, alphabet.size() + 1])
 
-
-            for i in range(0, len(features), n_steps):
-                chunk = features[i:i + n_steps]
-
+            # the frozen model only accepts input split to 16 step chunks,
+            # if the inference was run from checkpoint instead (as in single inference in deepspeech script), this loop wouldn't be needed
+            for i in range(0, features_len[0], n_steps):
+                chunk = features[:, i:i + n_steps, :, :]
+                chunk_length = chunk.shape[1];
                 # pad with zeros if not enough steps (len(features) % FLAGS.n_steps != 0)
-                if len(chunk) < n_steps:
+                if chunk_length < n_steps:
                     chunk = np.pad(chunk,
                                    (
-                                       (0, n_steps - len(chunk)),
+                                       (0, 0),
+                                       (0, n_steps - chunk_length),
                                        (0, 0),
                                        (0, 0)
                                    ),
                                    mode='constant',
                                    constant_values=0)
 
-                output = session.run(outputs['outputs'], feed_dict={
-                    inputs['input']: [chunk],
-                    inputs['input_lengths']: [len(chunk)],
+                # need to update the states with each loop iteration
+                logits_step, previous_state_c, previous_state_h = session.run([outputs['outputs'], outputs['new_state_c'], outputs['new_state_h']],feed_dict={
+                    inputs['input']: chunk,
+                    inputs['input_lengths']: [chunk_length],
+                    inputs['previous_state_c']: previous_state_c,
+                    inputs['previous_state_h']: previous_state_h,
                 })
-                logits = np.concatenate((logits, output))
 
+                logits = np.concatenate((logits, logits_step))
+
+            logits = np.squeeze(logits)
+
+            row_output = []
+            for j in range(args.predicted_character_count):
+                row_output.append([])
+
+            # now sort logits and turn them into characters + probabilities
             for i in range(0, len(logits)):
-                softmax_output = softmax(logits[i][0])
+                softmax_output = softmax(logits[i])
                 indexes_sorted = softmax_output.argsort()[args.predicted_character_count * -1:][::-1]
                 most_likely_chars = ''
                 chars_probability = ''
                 for j in range(args.predicted_character_count):
                     char_index = indexes_sorted[j]
                     if char_index < alphabet.size():
-                        text = alphabet.string_from_label(char_index)
+                        text = alphabet._string_from_label(char_index)
                         most_likely_chars += text+' '
+                        row_output[j].append(text)
                         chars_probability += ' (' + str(softmax_output[char_index]) + ')'
                     else:
                         most_likely_chars += '- '
+                        row_output[j].append('-')
                         chars_probability += ' (' + str(softmax_output[char_index]) + ')'
                 print(most_likely_chars, " ", chars_probability)
+
+            with open(args.input_file_path+"_acoustic.txt","w") as out:
+                for j in range(len(row_output)):
+                    out.write(', '.join(row_output[j])+"\n")
+                    print(row_output[j])
 
 
 if __name__ == '__main__':
     run_inference()
+
